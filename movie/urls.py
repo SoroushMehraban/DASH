731a82508304d9d0427d2e11b1e348f01469b894
Soroush Mehraban <soroush@vancosys.com>
2022-06-25 19:21:33 +0430

    Movie page added

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from movie.views import Home
+from movie.views import Home, Movies, NotExist404
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('movie/<int:movie_id>', Movies.as_view(), name="movies"),
+    path('404/', NotExist404.as_view(), name='NotExist404')
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Movie
+
 
 class Home(View):
     def get(self, request):
         return render(request, 'movie/home.html', context={
             'movies': Movie.objects.all()
         })
+
+
+class Movies(View):
+    def get(self, request, movie_id):
+        movie = Movie.objects.filter(id=movie_id).first()
+        if movie:
+            return render(request, 'movie/movie.html', context={'movie': movie})
+        else:
+            return redirect('NotExist404')
+
+
+class NotExist404(View):
+    def get(self, request):
+        return render(request, 'movie/404.html')
